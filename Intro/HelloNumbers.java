7c6f77191b87eb8b07560fd3cec9148e48a5e5ee
nirvana47 <nirvana47@gmail.com>
2019-03-02 23:29:40 -0800

    Modified HelloNumbers to print cumulative sum from 0 to 9

--- Intro/HelloNumbers.java
+++ Intro/HelloNumbers.java
@@ -1,5 +1,14 @@
-public class HelloNumbers{
-    public static void main(String[] args){
-System.out.println("Hello Numbers!");
+public class HelloNumbers {
+    public static void main(String[] args) {
+        System.out.println("Hello Numbers!");
+
+        int x = 0;
+        int y = 0;
+        while (x < 10) {
+            System.out.print(y + " ");
+            x = x + 1;
+            y=y+x;
+        }
+        System.out.println();
     }
 }
