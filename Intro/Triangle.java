369fc62e07672da90b3b2b39beea322f77350718
nirvana47 <nirvana47@gmail.com>
2019-03-03 00:42:40 -0800

    Modified Triangle.java as part of Creative Exercise 1(b) to print triangle based on user input

--- Intro/Triangle.java
+++ Intro/Triangle.java
@@ -1,11 +1,19 @@
 public class Triangle {
-    public static void main(String[] args) {
-        for (int x = 1; x <= 5; x++) {      //loop to go through 5 lines
-            for (int y = 0; y < x; y++) {   //loop to draw the triangle
-                System.out.print("*");
+    public static void drawTriangle(int x) {
+        if (x == 0 || x < 0) {
+            System.out.println("Invalid entry! 0 or less than zero not allowed.");
+        } else {
+            for (int counter = 1; counter <= x; counter++) {
+                for (int y = 0; y < counter; y++) {
+                    System.out.print("*");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        drawTriangle(10);
 
     }
 }
