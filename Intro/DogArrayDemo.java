04ea9d3a937a9785025048d3a58ec8000b5c6df3
nirvana47 <nirvana47@gmail.com>
2019-03-03 20:18:56 -0800

    Testing object creation in for loop

--- Intro/DogArrayDemo.java
+++ Intro/DogArrayDemo.java
@@ -5,6 +5,12 @@ public class DogArrayDemo {
         dogs[0] = new Dog(8);
         dogs[1] = new Dog(20);
 
+        Dog[] wolfDogs = new Dog[5];
+        for (int i = 0; i < 5; i++) {
+            wolfDogs[i] = new Dog(i * 23);
+            wolfDogs[i].makeNoise();
+        }
+
         /* Yipping will result, since dogs[0] has weight 8. */
         dogs[0].makeNoise();
     }
