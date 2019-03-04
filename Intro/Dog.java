9b7657c10fb9f497c2abdcd5f70cabf3b1a9cd57
nirvana47 <nirvana47@gmail.com>
2019-03-03 20:56:47 -0800

    Testing static vs non-static methods

--- Intro/Dog.java
+++ Intro/Dog.java
@@ -14,4 +14,18 @@ public class Dog {
             System.out.println("woof!");
         }
     }
+
+    public static Dog maxDog(Dog d1, Dog d2) {
+        if (d1.weightInPounds > d2.weightInPounds) {
+            return d1;
+        }
+        return d2;
+    }
+
+    // public static Dog maxDog(Dog d1, Dog d2) {
+    // if (weightInPounds > d2.weightInPounds) {
+    // return this;
+    // }
+    // return d2;
+    // }
 }
