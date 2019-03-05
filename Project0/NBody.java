2d493461151e077cf0c0fa5c359cba2660110ff0
nirvana47 <nirvana47@gmail.com>
2019-03-04 22:58:55 -0800

    Added definition for readBodies but returning null-pointer-exception. Needs further investigation.

--- Project0/NBody.java
+++ Project0/NBody.java
@@ -1,18 +1,28 @@
 public class NBody {
 
     public static double readRadius(String fileName) {
-        In in = new In(fileName);
-        int numOfPlanets = in.readInt();
-        double radiusUniverse = in.readDouble();
+        In fName = new In(fileName);
+        int numOfPlanets = fName.readInt();
+        double radiusUniverse = fName.readDouble();
 
         return radiusUniverse;
     }
 
     public static Body[] readBodies(String fileName) {
-        In in = new In(fileName);
-        int numOfPlanets = in.readInt();
-        double radiusUniverse = in.readDouble();
-        Body[] bodies = new Body[];
+        In fName = new In(fileName);
+        int numOfPlanets = fName.readInt();
+        double radiusUniverse = fName.readDouble();
+        Body[] bodies = new Body[5];
+        int counter = 0;
+        while (!fName.isEmpty() && counter < bodies.length) {
+            bodies[counter].xxPos = fName.readDouble();
+            bodies[counter].yyPos = fName.readDouble();
+            bodies[counter].xxVel = fName.readDouble();
+            bodies[counter].yyVel = fName.readDouble();
+            bodies[counter].mass = fName.readDouble();
+            bodies[counter].imgFileName = fName.readString();
+            counter += 1;
+        }
 
         return bodies;
     }
