b3c936c0043ee53ac83c5b674d83016ba2ad1613
artdayne <stewari@linkedin.com>
2019-03-08 00:51:20 -0800

    Added code to print out position of all the planets at end of simulation

--- Project0/NBody.java
+++ Project0/NBody.java
@@ -95,5 +95,12 @@ public class NBody {
 
             time += dT;
         }
+
+        StdOut.printf("%d\n", bodies.length);
+        StdOut.printf("%.2e\n", univRadius);
+        for (int i = 0; i < bodies.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n", bodies[i].xxPos, bodies[i].yyPos,
+                    bodies[i].xxVel, bodies[i].yyVel, bodies[i].mass, bodies[i].imgFileName);
+        }
     }
 }
