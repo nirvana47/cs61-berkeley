8f6f2d39ca4caf2926bcb2e55fc83be4c4653137
5tarfa11 <starfall.dayne@gmail.com>
2019-03-10 03:04:29 -0700

    Added iterativeSize method to find list size

--- Lists/IntList.java
+++ Lists/IntList.java
@@ -14,11 +14,25 @@ public class IntList{
 		return 1 + this.rest.size();
 	}
 	
+	public int iterativeSize(){
+		IntList p = this;
+		int totalSize = 0;
+		while (p!= null){
+			totalSize += 1;
+			p=p.rest;
+		}
+		return totalSize;
+	}
+	
+	
 	public static void main(String[] args){
 		IntList L = new IntList(15, null);
 		
 		L = new IntList(10,L);
 		L = new IntList(5,L);
+		
+		System.out.println(L.size());
+		System.out.println(L.iterativeSize());
 		
 	}
 }	
