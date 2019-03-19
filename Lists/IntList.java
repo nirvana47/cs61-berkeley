1029fcbc7fef9cf6be492174dc9dda5f7e600966
nirvana47 <nirvana47@gmail.com>
2019-03-18 23:02:28 -0700

    Added 3 new methods: (1)recursive concatenate (2)looping concatenate and (3) recursive lenght compare

--- Lists/IntList.java
+++ Lists/IntList.java
@@ -148,46 +148,110 @@ public class IntList {
      * B. May NOT modify items of A. Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        // TODO: fill in method
-        return null;
+        if (B == null || A == null) {
+            throw new IllegalArgumentException("One or both the IntLists are NULL");
+        }
+        IntList temp = new IntList(A.first, null);
+        IntList ptr = temp;
+        A = A.rest;
+        while (A != null) {
+            ptr.rest = new IntList(A.first, null);
+            ptr = ptr.rest;
+            A = A.rest;
+        }
+        while (B != null) {
+            ptr.rest = new IntList(B.first, null);
+            ptr = ptr.rest;
+            B = B.rest;
+        }
+
+        return temp;
+    }
+
+    /**
+     * Write the public static method named merge, which takes two parameters: each
+     * is a reference to a sorted (in increasing order) list. This method returns a
+     * reference to a new sorted list, which copies all the values from its two
+     * parameter lists (which remain unchanged). Hint: use a recursive structures
+     * limilar to the equalLength method described above
+     */
+    public static IntList recursiveCatenate(IntList A, IntList B) {
+        if (A == null && B != null) {
+            System.out.println("A null, B not null, calling recursiveCatenate(A, B.rest)");
+            return new IntList(B.first, recursiveCatenate(A, B.rest));
+        } else if (A != null && B == null) {
+            System.out.println("A not null, B null, calling recursiveCatenate(A.rest, B)");
+            return new IntList(A.first, recursiveCatenate(A.rest, B));
+        } else if (A == null && B == null) {
+            System.out.println("A null, B null, calling new IntList()");
+            return null;
+        } else {
+            System.out.println("A not null, B not null, calling recursiveCatenate(A.rest, B)");
+            return new IntList(A.first,recursiveCatenate(A.rest, B));
+        }
+    }
+
+    /**
+     * Examine the method below
+     * 
+     * public static boolean equalLength(LN l1, LN l2) {return length(l1) ==
+     * length(l2);}
+     * 
+     * Although this method is trivial to write, it takes a long time to execute
+     * when passed a very large linked list (a million node list) and a very short
+     * one (a 5 node list), because it computes the length of each separately, and
+     * computing the length of the large list takes much more time than computing
+     * the length of the small list. Rewrite this method directly using recursion so
+     * that it only traverses as much of each list as necessary to compute a result
+     * (when it tries to reach the 6th node of each list -and fails for the smaller
+     * list- it knows the answer. Hint: deal with the following four cases: both l1
+     * and l2 are empty, l1 is empty and l2 is not empty, l1 is not empty and l2 is
+     * empty, and finally both l1 and l2 are not empty; in three of four cases, an
+     * answer can be returned immediately (the other requires recursion).
+     */
+    public static boolean equalLength(IntList A, IntList B) {
+        if (A == null && B != null) {
+            return false;
+        } else if (A != null && B == null) {
+            return false;
+        } else if (A == null && B == null) {
+            return true;
+        } else {
+            return equalLength(A.rest, B.rest);
+        }
     }
 
     public static void main(String[] args) {
         IntList L = new IntList(15, null);
         L = new IntList(10, L);
         L = new IntList(5, L);
-        L = new IntList(0, L);
-        L = new IntList(-10, L);
-        L = new IntList(-5, L);
-        L = new IntList(-15, L);
+        // L = new IntList(0, L);
+        // L = new IntList(-10, L);
+        // L = new IntList(-5, L);
+        // L = new IntList(-15, L);
         L = new IntList(35, L);
-        int i = 3;
 
-        // L.printList();
+        IntList A = new IntList(10, null);
+        A = new IntList(13, A);
+        A = new IntList(14, A);
+        IntList B = new IntList(-10, null);
+        // IntList B = null;
+        B = new IntList(11, B);
+        B = new IntList(-11, B);
 
-        // dSquareList(L);
-        IntList M = squareListRecursive(L);
+        IntList C = recursiveCatenate(A, B);
 
-        System.out.println("Squared M List:");
-        M.printList();
+        dSquareList(A);
+        System.out.println("The C List:");
+        C.printList();
+        System.out.println("The A List:");
+        A.printList();
+        System.out.println("The B List:");
+        B.printList();
 
-        System.out.println("Original L List:");
-        L.printList();
+        // IntList D = catenate(A, C);
+        // System.out.println("The D List :");
+        // D.printList();
 
-        // incrList(L,i);
-        // System.out.println("Increment List method called");
-        // L.printList();
-
-        // System.out.println(L.size());
-        // System.out.println(L.iterativeSize());
-        // L.printList();
-        // System.out.println("The " + i + "'th value in this list is: " + L.get(i));
-
-        // Test your answers by uncommenting. Or copy and paste the
-        // code for incrList and dincrList into IntList.java and
-        // run it in the visualizer.
-        // System.out.println(L.get(1));
-        // System.out.println(incrList(L, 3));
-        // System.out.println(dincrList(L, 3));
     }
 }
