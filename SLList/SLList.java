5f42e2fed09b53affb8377aa2890b1d9c752ad69
nirvana47 <nirvana47@gmail.com>
2019-03-24 15:20:38 -0700

    Added size method for SLList with recursion

--- SLList/SLList.java
+++ SLList/SLList.java
@@ -1,34 +1,56 @@
-public class SLList{
+public class SLList {
 
     private IntNode first;
 
-    private class IntNode{
+    private static class IntNode {
         int item;
         IntNode next;
-    
-        public IntNode(int i, IntNode n){
+
+        public IntNode(int i, IntNode n) {
             item = i;
             next = n;
         }
     }
 
-    public SLList(int x){
+    public SLList(int x) {
         first = new IntNode(x, null);
     }
 
-    public void addFirst(int x){
-        first = new IntNode(x,first);
+    public void addFirst(int x) {
+        first = new IntNode(x, first);
     }
 
-    public int getFirst(){
+    public int getFirst() {
         return first.item;
+    }
+
+    public void addLast(int x) {
+        IntNode temp = first;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = new IntNode(x, null);
+    }
+
+    private static int size(IntNode n) {
+        if (n.next == null) {
+            return 1;
+        }
+        return 1 + size(n.next);
+    }
+
+    public int size() {
+        return size(first);
     }
 
     public static void main(String[] args) {
         SLList L1 = new SLList(25);
         L1.addFirst(15);
         L1.addFirst(5);
+        L1.addLast(10);
 
         System.out.println(L1.getFirst());
+        System.out.println(L1.size());
     }
 }
